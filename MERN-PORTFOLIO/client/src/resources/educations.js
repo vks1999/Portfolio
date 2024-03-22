77d305def6f2a4870211043b80fc676440906cc1
vks1999 <vsr2399@gmail.com>
2024-03-22 13:26:32 +0530

    done

--- MERN-PORTFOLIO/client/src/resources/educations.js
+++ MERN-PORTFOLIO/client/src/resources/educations.js
@@ -1,24 +1,24 @@
 export const educations = [
     {
         "_id": "48444346416346464611dc2",
-        "company": "GreetLabs Private Limited",
-        "title": "React Developer",
-        "period": "2021-Present",
-        "description": ""
+        "company": "Ambalika Institute Of Management And Technology",
+        "title": "Graduation",
+        "period": "2017-2022",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     },
     {
         "_id": "48444346416346464611dc2",
-        "company": "GreetLabs Private Limited",
-        "title": "React Developer",
-        "period": "2021-Present",
-        "description": ""
+        "company": "D.L.W. Inter College Varanasi",
+        "title": "Intermediate",
+        "period": "2014-2016",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam"
     },
     {
         "_id": "48444346416346464611dc2",
-        "company": "GreetLabs Private Limited",
-        "title": "React Developer",
-        "period": "2021-Present",
-        "description": ""
+        "company": "D.L.W. Inter College Varanasi",
+        "title": "High School",
+        "period": "2013-2014",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
     },
 
 ];
